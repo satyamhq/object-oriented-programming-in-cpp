a3bf5928711709522b50878ba681c5a27ff88ccb
Satyam Kumar <satyam31sk@gmail.com>
2026-08-26 11:20:12 +0530

    edit 19-employed-id-company-code.cpp

--- Unit 1/19-employed-id-company-code.cpp
+++ Unit 1/19-employed-id-company-code.cpp
@@ -1,18 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Employees {
-    public:
+public:
     static int employeeID;
     static int companyCode;
-        Employees() {
-            employeeID++;
-        }
-        void display() {
-            cout << "Employee ID: " << employeeID << endl;
-            cout << "Company Code: " << companyCode << endl;
-        }
+
+    Employees() {
+        employeeID++;
+    }
+
+    void display() {
+        cout << "Employee ID: " << employeeID << endl;
+        cout << "Company Code: " << companyCode << endl;
+    }
 };
 
+// Define and initialize static variables
 int Employees::employeeID = 0;
-int Employees::companyCode = 1234;
+int Employees::companyCode = 1001;
+
+int main() {
+    Employees e1;
+    e1.display();
+
+    Employees e2;
+    e2.display();
+
+    Employees e3;
+    e3.display();
+
+    return 0;
+}
